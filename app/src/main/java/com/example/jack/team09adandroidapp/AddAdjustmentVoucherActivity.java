616a9350bfa2658669e031dce9307a86ebb7e6b3
xuxu <yxs0109@gmail.com>
2018-01-29 20:21:42 +0800

    delete Main2Activity changed Item,ItemListActivity add 3 layout

--- app/src/main/java/com/example/jack/team09adandroidapp/AddAdjustmentVoucherActivity.java
+++ app/src/main/java/com/example/jack/team09adandroidapp/AddAdjustmentVoucherActivity.java
@@ -1,8 +1,11 @@
 package com.example.jack.team09adandroidapp;
 
+import android.app.Activity;
+
 /**
  * Created by CHEN ZIQING on 2018/1/28.
  */
 
-public class AddAdjustmentVoucherActivity {
+public class AddAdjustmentVoucherActivity extends Activity {
+
 }
--- app/src/main/java/com/example/jack/team09adandroidapp/Item.java
+++ app/src/main/java/com/example/jack/team09adandroidapp/Item.java
@@ -1,8 +1,67 @@
 package com.example.jack.team09adandroidapp;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
- * Created by e0224927 on 29/1/2018.
+ * Created by e0224927 on 28/1/2018.
  */
 
-public class Item {
+public class Item extends HashMap<String,String>{
+    final static String baseURL = "http://172.17.251.72:80/team09/AndroidServices/ItemService.svc/";
+
+
+    public Item(String itemID,String categoryID,String description,String location,
+                String unitOfMeasure,String reorderLevel,String reorderQty,String qtyOnHand){
+        put("ItemID",itemID);
+        put("CategoryID",categoryID);
+        put("Description",description);
+        put("Location",location);
+        put("UnitOfMeasure",unitOfMeasure);
+        put("ReorderLevel",reorderLevel);
+        put("ReorderQty",reorderQty);
+        put("QtyOnHand",qtyOnHand);
+
+    }
+
+    public static List<Item> getItemList() {
+        JSONArray a = JSONParser.getJSONArrayFromUrl(baseURL + "Item" );
+        List<Item> ItemList = new ArrayList<Item>();
+        try {
+            for (int i = 0; i < a.length(); i++) {
+                JSONObject b = a.getJSONObject(i);
+
+                ItemList.add(new Item(b.getString("ItemID"), b.getString("CategoryID"),
+                        b.getString("Description"),b.getString("Location"), b.getString("UnitOfMeasure"),
+                        b.getString("ReorderLevel"),b.getString("ReorderQty"),
+                        b.getString("QtyOnHand")));
+            }
+        } catch (Exception e) {
+
+        }
+        return (ItemList);
+    }
+
+    public static List<Item> getItemListByCAT(String category) {
+        JSONArray a = JSONParser.getJSONArrayFromUrl(baseURL + "Item/" + category);
+        List<Item> ItemList = new ArrayList<Item>();
+        try {
+            for (int i = 0; i < a.length(); i++) {
+                JSONObject b = a.getJSONObject(i);
+
+                ItemList.add(new Item(b.getString("ItemID"), b.getString("CategoryID"),
+                        b.getString("Description"),b.getString("Location"), b.getString("UnitOfMeasure"),
+                        b.getString("ReorderLevel"),b.getString("ReorderQty"),
+                        b.getString("QtyOnHand")));
+            }
+        }catch(Exception e){
+
+        }
+        return(ItemList);
+    }
+
 }
